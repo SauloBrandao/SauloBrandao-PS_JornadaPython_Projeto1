20dcad446d558fbc777e51713771b1a6d3219061
SauloBrandao <2023211510080@iesp.edu.br>
2025-09-17 00:02:15 -0300

    Feat: Terminando projeto aplicando o Pandas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import time # -> biblioteca do proprio Python responsavel por fazer controle de tempo
 import pandas
 
-pyautogui.PAUSE = 0.5 # -> definindo Delay entre os comandos
+pyautogui.PAUSE = 0.2 # -> definindo Delay entre os comandos
 
 pyautogui.press("win") # -> press serve pra apertar 1 tecla
 pyautogui.write("opera") # -> write serve pra escrever textos
@@ -30,5 +30,41 @@ time.sleep(3)
 
 tabela = pandas.read_csv("produtos.csv") # -> usando o pandas pra ler o CSV e armazenar as informações dentro da variavel Tabela
 
-pyautogui.click(x=753, y=280)
 
+
+for linha in tabela.index: # -> Loop pra prencher o formulario de produtos 
+# -> o pandas transforma o arquivo CSV em um DataFrame (Parecido com Excel)
+ # -> depois disso o pandas o .index vai linha por linha da tabela 
+    pyautogui.click(x=753, y=280)
+
+    codigo = tabela.loc[linha, "codigo"] # -> o loc serve pra especificar pegar a coluna, dentro vc passa 2 parametros, Linha (Linha da tabela) 
+    # e nome da coluna
+    pyautogui.write(codigo)
+
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+
+    pyautogui.press("tab")
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria)) # -> aqui é necessario mudar o tipo pra String para que possa ser escrito pela automação
+
+    pyautogui.press("tab")
+    preco_unitario = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco_unitario))
+
+    pyautogui.press("tab")
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    pyautogui.write(str(obs))
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(10000)
